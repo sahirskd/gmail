1fc44b70b93dbe019d956c55c67bfa2db91cda7c
Sahir khan <sahirskd@gmail.com>
2022-04-06 12:13:08 +0530

    implemented voice to text feature

--- src/Components/ComposeMail.js
+++ src/Components/ComposeMail.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
-import { Close, Delete, Minimize, Maximize } from '@mui/icons-material';
-import { Button, IconButton } from '@mui/material';
+import { Close, Delete, Minimize, Maximize, RecordVoiceOverRounded, VoiceOverOffRounded } from '@mui/icons-material';
+import { Button, Checkbox, IconButton, Tooltip } from '@mui/material';
 import './ComposeMail.css'
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeSendMessageDialog, minimizeComposeModal, maximizeComposeModal, selectComposeModalIsMin } from '../features/mailSlice';
 import { selectUser } from '../features/userSlice';
 import { serverTimestamp } from "firebase/firestore";
-import { db } from './firebase'
+import { db } from './firebase';
+import MicRecorder from 'mic-recorder-to-mp3';
 
 function ComposeMail() {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [micPermission, setmicPermission] = useState(false);
+    const [micCheckbox, setmicCheckbox] = useState(false)
+
+    // const recorder = new MicRecorder({ bitRate: 128 });
+    const [recorder, setrecorder] = useState(new MicRecorder({ bitRate: 128 }))
 
     const dispatch = useDispatch();
 
@@ -52,6 +58,102 @@ function ComposeMail() {
         dispatch(closeSendMessageDialog());
     }
 
+    const startStopRecording = (e) => {
+        console.log(e.target.checked)
+        if (!micPermission) {
+            return navigator.getUserMedia({ audio: true, video: false },
+                () => {
+                    console.log('Permission Granted');
+                    setmicPermission(true)
+                    setmicCheckbox(true)
+                    handleRecording(true)
+                },
+                () => {
+                    console.log('Permission Denied');
+                    setmicPermission(false)
+                    setmicCheckbox(false)
+                },
+            );
+        }
+        setmicCheckbox(!micCheckbox)
+
+        if (e.target.checked) {
+            handleRecording(true)
+        } else {
+            handleRecording(false)
+        }
+
+        // recorder.start().then(() => {
+        //     // something else
+        // }).catch((e) => {
+        //     console.error(e);
+        // });
+
+    }
+    const recordingAPI = async (url = '', data = {}) => {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            // credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+
+
+    const handleRecording = (startRec) => {
+        if (startRec) {
+            recorder.start().then(() => {
+                // console.log("started recording")
+                // something else
+            }).catch((e) => {
+                console.error(e);
+            });
+        } else {
+            recorder.stop().getMp3().then(([buffer, blob]) => {
+                // do what ever you want with buffer and blob
+                // Example: Create a mp3 file and play
+                const file = new File(buffer, 'song.mp3', {
+                    type: blob.type,
+                    lastModified: Date.now()
+                });
+
+
+                recordingAPI('http://localhost:3300/api/deepgram', { file: file })
+                    .then(data => {
+                        console.log(data); // JSON data parsed by `data.json()` call
+                    });
+
+                console.log(blob)
+                console.log(file)
+                console.log(URL.createObjectURL(file))
+                // const player = new Audio(URL.createObjectURL(file));
+                // player.play();
+
+
+            }).catch((e) => {
+                alert('We could not retrieve your message');
+                console.log(e);
+            });
+
+        }
+    }
+
+
+
+
+
+
+
     return (
         <div className={`compseMail ${dialogIsMinimized ? "composeMail--minimize" : "composeMail--maximize"}`}>
             <div className='composeMail__header'>
@@ -75,11 +177,17 @@ function ComposeMail() {
                 <textarea name='Message' type="text" placeholder='Message...' className='composeMail__message' {...register("Message", { required: "Message is required" })} />
                 {errors.Message && <p className='composeMail__errorMessage'>{errors.Message.message}</p>}
 
+
                 <div className='composeMail__footer'>
                     <Button className='composeMail__footerSend' type="submit" variant="contained">Send</Button>
-                    <IconButton onClick={() => dispatch(closeSendMessageDialog())}>
-                        <Delete className='composeMail__deleteIcon' />
-                    </IconButton>
+                    <div className='composeMail__footerRight'>
+                        <Tooltip title="Click for voice to text">
+                            <Checkbox icon={<VoiceOverOffRounded />} onChange={startStopRecording} checkedIcon={<RecordVoiceOverRounded />} checked={micCheckbox} size="small" />
+                        </Tooltip>
+                        <IconButton onClick={() => dispatch(closeSendMessageDialog())}>
+                            <Delete className='composeMail__deleteIcon' />
+                        </IconButton>
+                    </div>
                 </div>
             </form>
 
